9c0075bde040f62789191067d4c8ebed56ebc5b5
charizard <leafycharizard@gmail.com>
2023-03-09 20:38:25 +0530

    add the issue path

--- server/index.js
+++ server/index.js
@@ -1,19 +1,28 @@
 //@ts-check
-const express = require("express");
-const morgan = require("morgan");
-const multer = require("multer");
-const uuid = require("uuid").v4;
-const path = require("path");
-const crypto = require("crypto");
-const fs = require("fs");
-const fsp = require("fs/promises");
-const { cursorTo } = require("readline");
-const { nextTick } = require("process");
-const createHttpError = require("http-errors");
+// const express = require("express");
+import express from "express";
+import morgan from "morgan";
+// const morgan = require("morgan");
+// const multer = require("multer");
+import multer from "multer";
+// const uuid = require("uuid").v4;
+import { v4 as uuid } from "uuid";
+// const path = require("path");
+import path from "path";
+// const crypto = require("crypto");
+import crypto from "crypto";
+// const fsp = require("fs/promises");
+import fsp from "fs/promises";
+// const createHttpError = require("http-errors");
+import createHttpError from "http-errors";
+// const ipfsClient = require("ipfs-http-client");
+import { create } from "ipfs-http-client";
+// const fs = require("fs");
+import fs from "fs";
 
 //constants
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const UPLOADS_DIR = path.join(process.cwd(), "uploads");
 const NODE_ENV = process.env.NODE_ENV || "development";
 
@@ -29,10 +38,33 @@ async function sha256(filepath) {
   return hash;
 }
 
+/**
+ *
+ * @param {(req: import('express').Request,res: import('express').Response,next: import("express").NextFunction)=>Promise<any>} fn
+ * @returns {import("express").RequestHandler}
+ */
 function catchAsync(fn) {
   return function (req, res, next) {
     fn(req, res, next).catch(next);
   };
+}
+const ipfs = create({ url: "http://127.0.0.1:5001" });
+// const testFile = fs.readFileSync("./test.pdf");
+// ipfs.add(testFile).then((resp) => {
+//   console.log(resp);
+// });
+
+/**
+ * @param {string} filePath
+ */
+async function uploadFileToIPFS(filePath) {
+  // Read the file from disk
+  const file = fs.readFileSync(filePath);
+
+  // Upload the file to IPFS
+  const result = await ipfs.add(file);
+
+  return result.cid.toString();
 }
 
 //setup
@@ -86,6 +118,18 @@ if (NODE_ENV == "development") {
   });
 }
 
+if (NODE_ENV == "development") {
+  app.get("/issue", (req, res) => {
+    const template = `
+            <form action="/issue" method="post" enctype="multipart/form-data">
+                <input type="file" name="certificate" />
+                <button type="submit">issue</button>
+            </form>
+        `;
+    res.send(template);
+  });
+}
+
 app.post(
   "/calculatehash",
   upload.single("certificate"),
@@ -105,6 +149,23 @@ app.post(
     const hash = await sha256(req.file.path);
     await fsp.unlink(filepath);
     res.send(`hash: ${hash}`);
+  })
+);
+
+app.post(
+  "/issue",
+  upload.single("certificate"),
+  catchAsync(async (req, res, next) => {
+    if (!req.file) {
+      throw new createHttpError.BadRequest("file not found");
+    }
+    const hash = await sha256(req.file.path);
+    const cid = await uploadFileToIPFS(req.file.path);
+    return res.json({
+      hash: hash,
+      cid: cid,
+      ifpsLink: `https://ipfs.io/ipfs/${cid}`,
+    });
   })
 );
 
